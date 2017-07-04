b6a46b3d2e52368fbb7dbe2591aa2b86f1e7d86f
maurelian <maurelian@protonmail.ch>
2017-07-04 08:21:40 -0400

    Updates for solidity-coverage configuration

---
+++ .solcover.js
@@ -0,0 +1,4 @@
+module.exports = {
+    testrpcOptions: '--networkId 50',
+    // testCommand: 'npm run test',
+};
--- truffle.js
+++ truffle.js
@@ -10,7 +10,14 @@ module.exports = {
       port: 8546,
       network_id: '42',
       gas: 4612388,
-    },
+    }, 
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- Use port 8555  
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    }
   },
   test_directory: 'transpiled/test',
   migrations_directory: 'transpiled/migrations',
