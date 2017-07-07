aa8e7f4317ceb34baff100e3eef6ab50439e9562
maurelian <maurelian@protonmail.ch>
2017-07-07 14:36:32 -0400

    set norpc, and run testrpc manually

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,5 @@
 module.exports = {
     testrpcOptions: '--networkId 50',
-    testCommand: `${process.env.PWD}/node_modules/truffle/cli.js test`,
+    testCommand: `${process.env.PWD}/node_modules/truffle/cli.js test --network coverage`,
+    norpc: true,
 };
