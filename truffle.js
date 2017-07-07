542e6f8de0d2647d8e7277596001b8838b7e1a49
maurelian <leighvert@protonmail.ch>
2017-07-07 10:14:27 -0400

    MOAR GAS!!

--- truffle.js
+++ truffle.js
@@ -15,7 +15,7 @@ module.exports = {
       host: "localhost",
       network_id: "*",
       port: 8555,         // <-- Use port 8555  
-      gas: 0xfffffffffff, // <-- Use this high gas value
+      gas: 0xfffffffffffffff, // <-- Use this high gas value
       gasPrice: 0x01      // <-- Use this low gas price
     }
   },
